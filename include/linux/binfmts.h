a462f03c5395e9535002183e8821320eff417656
Flopster101 <nahuelgomez329@gmail.com>
2025-04-01 11:56:05 -0300

    include: binfmts: extend task_is_booster with more HALs and services
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -166,7 +166,11 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	       !strcmp(comm, "perf@2.2-servic") ||
 	       !strcmp(comm, "power@2.0-servic") ||
 	       !strcmp(comm, "iop@") ||
-	       !strcmp(comm, "init.qcom.post_");
+	       !strcmp(comm, "PERFD-SERVER") ||
+	       !strcmp(comm, "init.qcom.post_") ||
+	       !strcmp(comm, "mtkPowerMsgHdl") ||
+	       !strcmp(comm, "mtkPowerService") ||
+	       !strncmp(comm, "system_perf_ini", 9);
 }
 
 static inline bool task_controls_frequencies(struct task_struct *tsk)
